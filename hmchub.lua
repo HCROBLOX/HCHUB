7711270d128617cddbc2164b8172f1e9fb928750
HCROBLOX <lekhanhhung4444@gmail.com>
2026-04-28 19:57:59 +0700

    Add files via upload

--- hmchub.lua
+++ hmchub.lua
@@ -21,7 +21,7 @@ loadstring(game:HttpGet(("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V
   })
 
        MinimizeButton({
-       Image = "http://www.roblox.com/asset/?id=96816291733751",
+       Image = "http://www.roblox.com/asset/?id=86718689170693",
        Size = {60, 60},
        Color = Color3.fromRGB(10, 10, 10),
        Corner = true,
@@ -31,13 +31,15 @@ loadstring(game:HttpGet(("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V
       
 ------ Tab
      local Tab1o = MakeTab({Name = "Script Ngon"})
+     local Tab2o = MakeTab({Name = "Script Farm"})
+     local Tab3o = MakeTab({Name = "Script Hop Sever"})
      
 ------- BUTTON
     
     AddButton(Tab1o, {
      Name = "Trẩu Hub",
     Callback = function()
-	  loadstring(game:HttpGet("https://raw.githubusercontent.com/trungdao2k4/trauroblox/refs/heads/main/traubluev1"))()
+	  loadstring(game:HttpGet("https://raw.githubusercontent.com/realkidhub/realkid/refs/heads/main/main.lua"))()
   end
   })
   AddButton(Tab1o, {
@@ -52,3 +54,30 @@ loadstring(game:HttpGet(("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V
     loadstring(game:HttpGet("https://raw.githubusercontent.com/XNEOFF/FlyGuiV3/main/FlyGuiV3.txt"))()
     end
     })
+  AddButton(Tab2o, {
+     Name = "Quantum Hub",
+    Callback = function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/flazhy/QuantumOnyx/refs/heads/main/QuantumOnyx.lua"))()
+    end
+    })
+  AddButton(Tab2o, {
+     Name = "Gravity Hub",
+    Callback = function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Dev-GravityHub/BloxFruit/refs/heads/main/Main.lua"))()
+    end
+    })
+    AddButton(Tab2o, {
+     Name = "Doraemon Hub",
+    Callback = function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/KiddoHiru/BloxFruits/main/MasterHub.lua"))()
+    end
+    })
+    AddButton(Tab3o, {
+     Name = "Night Hop Boss",
+    Callback = function()
+    loadstring(game:HttpGet("https://github.com/WhiteX1208/Scripts/blob/main/HopScript.luau?raw=true"))()
+    end
+    })
+  
+    
+    
